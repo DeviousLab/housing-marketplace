5e97352c543df80f5b8c342a5ba8126e16c44cb0
DeviousLab <deviouslab@gmail.com>
2022-04-10 23:30:22 +0400

    Implemented sign in and sign up pages
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,51 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Sign In</div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome back!</p>
+        </header>
+        <form action="">
+          <input type="email" name="email" id="email" className='emailInput' placeholder='Email Address' value={email} onChange={onChange} />
+          <div className='passwordInputDiv'>
+            <input type={showPassword ? 'text' : 'password'} name="password" id="password" className='passwordInput' placeholder='Password' value={password} onChange={onChange} />
+            <img src={visibilityIcon} alt='Show Password?' className='showPassword' onClick={() => setShowPassword((prevState) => !prevState)} />
+          </div>
+          <Link to='/forgotpassword' className='forgotPasswordLink'>Forgot Password?</Link>
+          <div className="signInBar">
+            <p className="signInText">
+              Sign In
+            </p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#fff' width='36px' height='36px' />
+            </button>
+          </div>
+        </form>
+
+        <Link to='/signup' className='registerLink'>Don't have an account?</Link>
+      </div>
+    </>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,52 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+  const { name, email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Sign Up</div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome!</p>
+        </header>
+        <form>
+          <input type="text" name="name" id="name" className='nameInput' placeholder='First Name' value={name} onChange={onChange} />
+          <input type="email" name="email" id="email" className='emailInput' placeholder='Email Address' value={email} onChange={onChange} />
+          <div className='passwordInputDiv'>
+            <input type={showPassword ? 'text' : 'password'} name="password" id="password" className='passwordInput' placeholder='Password' value={password} onChange={onChange} />
+            <img src={visibilityIcon} alt='Show Password?' className='showPassword' onClick={() => setShowPassword((prevState) => !prevState)} />
+          </div>
+          <div className="signUpBar">
+            <p className="signUpText">
+              Sign Up
+            </p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#fff' width='36px' height='36px' />
+            </button>
+          </div>
+        </form>
+
+        <Link to='/signin' className='registerLink'>Sign in instead?</Link>
+      </div>
+    </>
   )
 }
 
