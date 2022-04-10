095b95c0354524c0f06cfa505fe872426f27fd2a
DeviousLab <deviouslab@gmail.com>
2022-04-10 22:39:00 +0400

    Created pages for project
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
     <div>
