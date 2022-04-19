9c2dc12cb5e49fb8cfadc576942c807c1a4f2a53
DeviousLab <deviouslab@gmail.com>
2022-04-20 03:15:40 +0400

    Switched to using text field for property address
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -93,7 +93,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     const storeImage = async (image) => {
@@ -134,9 +133,9 @@ function CreateListing() {
     });
 
     const formDataCopy = { ...formData, geolocation, imgUrls, timestamp: serverTimestamp()};
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
